aaf16c14e4641360345022dc8c858184c076c57b
sateesh <masabhathini@gmail.com>
2022-07-05 15:07:03 +0300

    Update condaCreate.sh

--- condaCreate.sh
+++ condaCreate.sh
@@ -27,9 +27,9 @@ conda install  -n envPy38Sat -c conda-forge geopandas   -y
 conda install  -n envPy38Sat -c conda-forge rioxarray   -y
 conda install  -n envPy38Sat -c conda-forge climpred   -y
 conda install  -n envPy38Sat -c conda-forge xclim   -y
-conda install  -n envPy38sat -c anaconda jinja2
-conda install  -n envPy38sat -c conda-forge folium
-conda install  -n envPy38sat -c conda-forge qgis
+conda install  -n envPy38Sat -c anaconda jinja2
+conda install  -n envPy38Sat -c conda-forge folium
+conda install  -n envPy38Sat -c conda-forge qgis
 #######################################
 #######################################
 source /home/$HOME/anaconda3/bin/activate envPy38Sat
