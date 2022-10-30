97c7dfcb6bd54a96d6afd11f01ea536c9f419a78
sateesh <masabhathini@gmail.com>
2022-10-30 12:16:28 +0300

    Update condaCreate.sh

--- condaCreate.sh
+++ condaCreate.sh
@@ -26,6 +26,7 @@ conda install  -n envPy38Sat -c conda-forge basemap-data-hires   -y
 conda install  -n envPy38Sat -c conda-forge geopandas   -y
 conda install  -n envPy38Sat -c conda-forge rioxarray   -y
 conda install  -n envPy38Sat -c conda-forge gdal -y
+conda install  -n envPy38Sat -c conda-forge pycrs -y
 conda install  -n envPy38Sat -c conda-forge climpred   -y
 conda install  -n envPy38Sat -c conda-forge xclim   -y
 conda install  -n envPy38Sat -c conda-forge windrose -y
