c25b1cf2e4215523c441874e7d766a3a4783c7a3
sateesh <masabhathini@gmail.com>
2023-11-27 23:33:35 +0300

    Update miniCondaCreate.sh
    Updated based on wrf-python.

--- miniCondaCreate.sh
+++ miniCondaCreate.sh
@@ -6,24 +6,25 @@
 #~/miniconda3/bin/conda init bash
 #~/miniconda3/bin/conda init zsh
 ########################## After this reopen the terminal ##############
-conda create -n envPy38Sat -c conda-forge  -y
-conda activate envPy38Sat
-conda install  -n envPy38Sat -c conda-forge metpy    -y
+## conda create -n envPy311Sat -c conda-forge  -y
+############################################################
+conda create  -n envPy311Sat -c conda-forge wrf-python -y
+conda activate envPy311Sat
+conda install  -n envPy311Sat -c conda-forge metpy    -y
 ### Big Data parallel computing 
-conda install  -n envPy38Sat -c conda-forge dask distributed  dask-jobqueue  -y
+conda install  -n envPy311Sat -c conda-forge dask distributed  dask-jobqueue  -y
 ####  Tools 
-conda install  -n envPy38Sat -c conda-forge python-cdo  -y
+conda install  -n envPy311Sat -c conda-forge python-cdo  -y
 ## plotting
-conda install  -n envPy38Sat -c conda-forge matplotlib   -y
+conda install  -n envPy311Sat -c conda-forge matplotlib   -y
 ## Shape files
-conda install  -n envPy38Sat -c conda-forge basemap   -y
-conda install  -n envPy38Sat -c conda-forge cartopy   -y
-conda install  -n envPy38Sat -c conda-forge basemap-data-hires   -y
-conda install  -n envPy38Sat -c conda-forge geopandas   -y
-conda install  -n envPy38Sat -c conda-forge rioxarray   -y
-conda install  -n envPy38Sat -c conda-forge gdal -y
-conda install  -n envPy38Sat -c conda-forge wrf-python -y
-pip install geog
+conda install  -n envPy311Sat -c conda-forge basemap   -y
+## conda install  -n envPy311Sat -c conda-forge cartopy   -y
+## conda install  -n envPy311Sat -c conda-forge basemap-data-hires   -y
+conda install  -n envPy311Sat -c conda-forge geopandas   -y
+conda install  -n envPy311Sat -c conda-forge rioxarray   -y
+## conda install  -n envPy311Sat -c conda-forge gdal -y
+## pip install geog
 pip install salem
 pip install python-dotenv
 pip install motionless
