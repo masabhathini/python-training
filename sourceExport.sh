381bdedf306f9bd72f6b7e6d4ac959565f3b9a53
sateesh <masabhathini@gmail.com>
2025-12-07 21:04:22 +0300

    Create sourceExport.sh
    kaust workstations.

---
+++ sourceExport.sh
@@ -0,0 +1,32 @@
+module load  hdf5/1.10.7/gcc-9.3.0-openmpi-prxludw
+module load  libaec/1.0.5/gcc-9.3.0-x7qsckk
+module load  numactl/2.0.14/gcc-9.3.0-dvypjdq
+module load  openmpi/4.1.1/gcc-9.3.0-4fwmxhl
+module load  hwloc/2.6.0/gcc-9.3.0-bshjcat
+module load  libpciaccess/0.16/gcc-9.3.0-oqupobb
+module load  libxml2/2.9.12/gcc-9.3.0-wpwqzco
+module load  libiconv/1.16/gcc-9.3.0-ipwffmp
+module load  xz/5.2.5/gcc-9.3.0-pjas6xo
+module load  zlib/1.2.11/gcc-9.3.0-cvyc3qq
+module load  ncurses/6.2/gcc-9.3.0-w76okcg
+module load  libevent/2.1.12/gcc-9.3.0-zh55wbb
+module load  openssl/1.1.1l/gcc-9.3.0-ho5gdg4
+module load  openssh/8.7p1/gcc-9.3.0-teg3lqc
+module load  libedit/3.1-20210216/gcc-9.3.0-c4w72u3
+module load  pkgconf/1.8.0/gcc-9.3.0-eihxo45
+module load  jasper/2.0.32/gcc-9.3.0-mekp25s
+module load  libjpeg-turbo/2.1.0/gcc-9.3.0-5aism6m
+module load  libpng/1.6.37/gcc-9.3.0-7it5bai
+module load  libtirpc/1.2.6/gcc-9.3.0-hkfxugz
+module load  krb5/1.19.2/gcc-9.3.0-v3ed7hv
+module load  netcdf-c/4.8.1/gcc-9.3.0-openmpi-zbs6ris
+module load  netcdf-fortran/4.5.3/gcc-9.3.0-openmpi-4l575it
+module load  parallel-netcdf/1.12.2/gcc-9.3.0-openmpi-iuh4c7j
+module load  perl/5.34.0/gcc-9.3.0-wy2a6t6
+module load  berkeley-db/18.1.40/gcc-9.3.0-xjhkkxa
+module load  bzip2/1.0.8/gcc-9.3.0-45ekva3
+module load  gdbm/1.19/gcc-9.3.0-2ap5kcu
+module load  readline/8.1/gcc-9.3.0-4wkig23
+export NETCDF=/sw/workstations/apps/linux-ubuntu20.04-broadwell/netcdf-fortran/4.5.3/gcc-9.3.0/4l575ityuxqcl7iytehhm2pmlfjb643k/
+export JASPERINC=/sw/workstations/apps/linux-ubuntu20.04-broadwell/jasper/2.0.32/gcc-9.3.0/mekp25ssnjbw37upxiyw7zfrwp4knnmk/include
+export JASPERLIB=/sw/workstations/apps/linux-ubuntu20.04-broadwell/jasper/2.0.32/gcc-9.3.0/mekp25ssnjbw37upxiyw7zfrwp4knnmk/lib
