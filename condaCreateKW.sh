bb9b86356ceb660e262112babc9e52b8e58691fc
sateesh <masabhathini@gmail.com>
2025-12-07 23:04:38 +0300

    Update condaCreateKW.sh

--- condaCreateKW.sh
+++ condaCreateKW.sh
@@ -1,3 +1,3 @@
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-conda install -c conda-forge wgrib wgrib2
-conda install -c conda-forge cdo eccodes netcdf4 hdf5
+conda create -n envwgrib -c conda-forge wgrib wgrib2
+conda install -n envwgrib -c conda-forge cdo eccodes netcdf4 hdf5
